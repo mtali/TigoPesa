dbeef211387fecb0726f44dbfd3a29d50a39d8d2
mtali <emmanuelmtali@yahoo.com>
2023-03-04 21:26:10 +0300

    Configure hilt entry points

--- app/src/main/java/com/mtali/tigopesa/app/main/MainActivity.kt
+++ app/src/main/java/com/mtali/tigopesa/app/main/MainActivity.kt
@@ -9,9 +9,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mtali.tigopesa.ui.theme.TigoPesaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
